b64e4314e8093c5f8af8d3210e2fbe355c922da5
inesmouquinho <84418425+inesmouquinho@users.noreply.github.com>
2021-05-19 23:10:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function compute()
+function compute() {
+    //Get the values and calculate 
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var yearInTheFuture = new Date().getFullYear() + years;
+
+    //Text result
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+            "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+            "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+            "in the year <mark>" + yearInTheFuture + "</mark>";
+    }
+//update the rate value
+function getSliderValue() 
 {
-    p = document.getElementById("principal").value;
-    
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
-        
+
+//Check for positive values
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var positive = parseInt(principal) > 0;
+    if (!positive) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+
+}
